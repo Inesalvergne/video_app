1398d853c9b66cdb58d4475bc39f3356600125ac
Ines Alvergne <i.alvergne@gmail.com>
2024-01-23 13:05:10 +0100

    create simple seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Video.destroy_all
+
+puts "Creating videos..."
+video_1 = {title: "Video 1", description: "wohoo!" }
+video_2 =  {title: "Video 2", description: "trop bien"}
+
+[video_1, video_2].each do |attributes|
+  video = Video.create!(attributes)
+  puts "Created #{video.title}"
+end
+
+puts "The seed is ready ✅"
